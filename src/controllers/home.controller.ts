aab14062a02f8d22806b41e8d7e6fb478b57c28f
Shorya Jain <shoryajain0708@gmail.com>
2024-06-04 22:45:16 +0530

    master-home

--- src/controllers/home.controller.ts
+++ src/controllers/home.controller.ts
@@ -4,6 +4,7 @@ import { listCategories } from "../util/home.util";
 import { ApiResponse } from "../util/apiResponse";
 import { ResponseStatusCode } from "../constants/constants";
 import { ParaExpert } from "../models/paraExpert/paraExpert.model";
+import { User } from "../models/user/user.model";
 import { paraExpertTypes } from "../models/paraExpert/paraExpert.types";
 
 export const getCategories = asyncHandler(
@@ -24,14 +25,13 @@ export const getSearchResults = asyncHandler(
     try {
       const { searchQuery } = req.body;
 
-      let paraExperts:any = await ParaExpert.find({
+      let paraExperts: any = await ParaExpert.find({
         $or: [
           { name: { $regex: searchQuery, $options: "i" } },
           { expertise: { $in: searchQuery } },
           { expertise: { $elemMatch: { $in: [searchQuery] } } },
         ],
       });
-
 
       return res.json(new ApiResponse(ResponseStatusCode.SUCCESS, paraExperts));
     } catch (error) {
@@ -42,16 +42,24 @@ export const getSearchResults = asyncHandler(
   }
 );
 
-export const getAll = asyncHandler(
-  async(req:Request, res:Response)=>{
-    try{
-      const categories = listCategories();
-      const paraExperts:any = await ParaExpert.find().limit(10) 
-      return res.json(new ApiResponse(ResponseStatusCode.SUCCESS, {categories, paraExperts}));
-    } catch(error){
-      return res.json(
-        new ApiResponse(ResponseStatusCode.INTERNAL_SERVER_ERROR, error.message)
+export const getAll = asyncHandler(async (req: Request, res: Response) => {
+  try {
+    const categories = listCategories();
+    const paraExperts: paraExpertTypes[] = await ParaExpert.find().limit(10);
+
+    const paraExpertUsers: { expertDetails: paraExpertTypes; userDetails: UserTypes }[] =
+      await Promise.all(
+        paraExperts.map(async (paraExpert) => {
+          const user = await User.findById(paraExpert.userId);
+          return { expertDetails:paraExpert, userDetails:user };
+        })
       );
-    }
+    return res.json(
+      new ApiResponse(ResponseStatusCode.SUCCESS, { categories, paraExperts:paraExpertUsers })
+    );
+  } catch (error) {
+    return res.json(
+      new ApiResponse(ResponseStatusCode.INTERNAL_SERVER_ERROR, error.message)
+    );
   }
-)
+});
