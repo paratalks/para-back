24b0c0c67bb52ad5e9c84fdb9da194f3bb594cad
Amar kumar <aknbo30@gmail.com>
2024-07-16 19:23:53 +0530

    created  appointments API for admin

--- src/routes/v1/admin.route.ts
+++ src/routes/v1/admin.route.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { getUsers,getParaExpert,getParaExpertByID,paraExpertSignup} from "../../controllers/admin.controller"
+import { getUsers,getParaExpert,getParaExpertByID,paraExpertSignup,getAppointments} from "../../controllers/admin.controller"
 import { hasAdminAccess } from "../../middlewares/auth.middleware";
 
 const router = express.Router();
@@ -9,5 +9,7 @@ router.route("/get-all/users").get(hasAdminAccess,getUsers);
 router.route("/get-all/paraExpert").get(hasAdminAccess,getParaExpert);
 router.route("/get-paraExpert/:paraExpertID").get(hasAdminAccess,getParaExpertByID);
 router.route("/paraExpert/signup").post(hasAdminAccess,paraExpertSignup);
+router.route("/get-all/appointments").get(hasAdminAccess,getAppointments);
+
 
 export default router;
