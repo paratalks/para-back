daafe3ee6d92065f9720a81227fb6eed5c9ce278
yash24shukla <thisform240@gmail.com>
2024-07-18 15:49:32 +0530

    minor typo mistake fix

--- src/models/packageBooking/packageBooking.types.ts
+++ src/models/packageBooking/packageBooking.types.ts
@@ -1,6 +1,6 @@
 import { Schema, Document, Model } from 'mongoose';
 
-export  interface  PackageBookingTpes {
+export  interface  PackageBookingTypes {
   packageId: Schema.Types.ObjectId;
   paraExpertId: Schema.Types.ObjectId; 
   userId: Schema.Types.ObjectId;
@@ -12,6 +12,6 @@ export  interface  PackageBookingTpes {
 }
 
 
-export interface PackageBookingDocument extends PackageBookingTpes, Document {}
+export interface PackageBookingDocument extends PackageBookingTypes, Document {}
 
 export interface PackageBookingModle extends Model<PackageBookingDocument> {}
