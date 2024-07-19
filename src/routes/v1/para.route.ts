d3a0984a37a317d044503ee94daadeec9d89f740
Amar kumar <aknbo30@gmail.com>
2024-07-19 11:33:35 +0530

    Implement Get Packages API for Para Expert App

--- src/routes/v1/para.route.ts
+++ src/routes/v1/para.route.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import { getBookings, getParaExpertAvailability } from "../../controllers/appointment.controller";
 import { verifyJWT } from "../../middlewares/auth.middleware";
-import { setAvailability, getAvailability,createAndUpdateExpertPackages} from "../../controllers/userUpdate.controller";
+import { setAvailability, getAvailability,createAndUpdateExpertPackages,getExpertPackages} from "../../controllers/userUpdate.controller";
 const router = express.Router();
 
 router.route("/get-bookings").get(verifyJWT,getBookings);
@@ -9,6 +9,7 @@ router.route("/get-available-slots").get(verifyJWT,getParaExpertAvailability);
 router.route("/set-availability").patch(verifyJWT,setAvailability);
 router.route("/get-availability").get(verifyJWT,getAvailability);
 router.route("/create-and-update-packages").patch(verifyJWT,createAndUpdateExpertPackages);
+router.route("/get-packages").get(verifyJWT,getExpertPackages);
 
 
 export default router;
